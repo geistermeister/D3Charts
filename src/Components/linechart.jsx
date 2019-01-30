0c1ffb6d78e2a9c5f6aa98754b1aae1f10e30a52
geistermeister <dermitdemdingtanzt@hotmail.de>
2019-01-30 17:04:40 +0100

    again

--- src/Components/linechart.jsx
+++ src/Components/linechart.jsx
@@ -11,19 +11,23 @@ export default class LineCHart extends Component {
     const margin = {top: 20, right: 30, bottom: 45, left: 50}
     const width = this.props.width - margin.left - margin.right
     const height = this.props.height - margin.top - margin.bottom
-
     let parseTime = d3.timeParse('%d.%m.%Y')
-
+    // IAM, DCI, HOLDING
+    let colors = ["#D31409", "#0068B4", "#E05206"]
     let buffer = []
     let bufferline
     let maxX = 0
     let data = this.props.data.data
+    let keys = Object.keys(data[0])
 
+    // copy data object into array, 
     data.forEach((d,i) => {
       buffer.push(Object.values(d))
+      // format the date string into date type
       buffer[i][0] = parseTime(buffer[i][0])
       for(let j = 1; j < buffer[0].length; j++){
         if(typeof buffer[i][j] != 'number'){
+          // number string into int
           buffer[i][j] = parseInt(buffer[i][j])
           if(buffer[i][j] > maxX){
             maxX = buffer[i][j]
@@ -31,8 +35,6 @@ export default class LineCHart extends Component {
         }
       }
     })
-
-    console.log(maxX)
 
     // manipulate svg and create group
     let svg = d3.select('svg')
@@ -42,7 +44,8 @@ export default class LineCHart extends Component {
       .attr('transform',
             `translate(${margin.left},${margin.top})`)
   
-
+    // splits data array into arrays
+    // e.g. array[date, value1, value2] = array[date, value1] & array[date, value2] 
     for(let i = 1; i < buffer[0].length; i++){
       bufferline = []
       for(let j = 0; j < buffer.length; j++){
@@ -61,31 +64,36 @@ export default class LineCHart extends Component {
         .domain([0, maxX])
         .range([height, 0])
 
-      // add the x axis
-      svg.append('g')
-        .attr('transform', `translate(0,${height})`)
-        .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%d.%m.%Y")))
-
-      // add the y axis
-      svg.append('g')
-        .call(d3.axisLeft(y))
-
       // define the line
       let line = d3.line()
         .x(d => x(d[0]))
         .y(d => y(d[1]))
         .curve(d3.curveMonotoneX)
-        
+      
       // add the line
-      svg.append("g")
+      svg.append('g')
         .attr('class', 'lineContainer')
         .append('path')
         .data([bufferline])
         .attr('class', 'line')
         .attr('d', line)
         .attr('fill', 'none')
-        .attr('stroke', 'steelblue')
+        .attr('stroke', colors[i-1])
         .attr('stroke-width', '1.5')
+        // append text for line when mouse over
+        .on('mouseover', () => {
+          svg.append('text')
+            .attr('class', 'title')
+            .attr('font-family', 'sans-serif')
+            .attr('font-weight', 'bold')
+            .attr('fill', colors[i-1])
+            .text(keys[i])
+            .attr('x', width/2)
+            .attr('y', 5)
+        })
+        .on('mouseout', () => {
+          svg.select('.title').remove()
+        })
         
       // add the dots  
       svg.selectAll('.lineContainer').selectAll('circle')
@@ -95,7 +103,16 @@ export default class LineCHart extends Component {
         .attr('cx', d => x(d[0]))
         .attr('cy', d => y(d[1]))
         .attr('r', '3')
-        .attr('fill', 'steelblue')  
+        .attr('fill', colors[i-1])  
+
+      // add the x axis
+      svg.append('g')
+        .attr('transform', `translate(0,${height})`)
+        .call(d3.axisBottom(x).tickFormat(d3.timeFormat('%d.%m.%Y')))
+
+      // add the y axis
+      svg.append('g')
+        .call(d3.axisLeft(y))
 
     } 
     
