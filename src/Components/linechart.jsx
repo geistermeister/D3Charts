2da698f02264ecd863a1113ffdff9e6ce671b1ed
geistermeister <dermitdemdingtanzt@hotmail.de>
2019-01-30 16:20:07 +0100

    linechart displays all lines in one datafile

--- src/Components/linechart.jsx
+++ src/Components/linechart.jsx
@@ -15,6 +15,8 @@ export default class LineCHart extends Component {
     let parseTime = d3.timeParse('%d.%m.%Y')
 
     let buffer = []
+    let bufferline
+    let maxX = 0
     let data = this.props.data.data
 
     data.forEach((d,i) => {
@@ -23,26 +25,14 @@ export default class LineCHart extends Component {
       for(let j = 1; j < buffer[0].length; j++){
         if(typeof buffer[i][j] != 'number'){
           buffer[i][j] = parseInt(buffer[i][j])
+          if(buffer[i][j] > maxX){
+            maxX = buffer[i][j]
+          }
         }
       }
     })
 
-    console.log(buffer)
-
-    let x = d3.scaleTime()
-      .domain(d3.extent(buffer, d => d[0]))
-      .range([0, width])
-
-
-    let y = d3.scaleLinear()
-      .domain([0, d3.max(buffer, d => d[1])])
-      .range([height, 0])
-
-    // define the line
-    let line = d3.line()
-      .x(d => x(d[0]))
-      .y(d => y(d[1]))
-      .curve(d3.curveMonotoneX)
+    console.log(maxX)
 
     // manipulate svg and create group
     let svg = d3.select('svg')
@@ -52,32 +42,62 @@ export default class LineCHart extends Component {
       .attr('transform',
             `translate(${margin.left},${margin.top})`)
   
-    // add the x axis
-    svg.append('g')
-      .attr('transform', `translate(0,${height})`)
-      .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%d.%m.%Y")))
-  
-    // add the y axis
-    svg.append('g')
-      .call(d3.axisLeft(y))
 
-    // add the line
-    svg.append('path')
-      .data([buffer])
-      .attr('class', 'line')
-      .attr('d', line)
-      .attr('fill', 'none')
-      .attr('stroke', 'steelblue')
-      .attr('stroke-width', '1.5')
+    for(let i = 1; i < buffer[0].length; i++){
+      bufferline = []
+      for(let j = 0; j < buffer.length; j++){
+        let fusionArray = []
+        fusionArray[0] = buffer[j][0]
+        fusionArray[1] = buffer[j][i]
+        bufferline.push(fusionArray)
+      }
 
-    svg.selectAll('circle')
-      .data(buffer).enter()
-      .append('circle')
-      .attr('class', 'dot')
-      .attr('cx', d => x(d[0]))
-      .attr('cy', d => y(d[1]))
-      .attr('r', '3')
-      .attr('fill', 'steelblue')
+      let x = d3.scaleTime()
+      .domain(d3.extent(buffer, d => d[0]))
+      .range([0, width])
+
+
+      let y = d3.scaleLinear()
+        .domain([0, maxX])
+        .range([height, 0])
+
+      // add the x axis
+      svg.append('g')
+        .attr('transform', `translate(0,${height})`)
+        .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%d.%m.%Y")))
+
+      // add the y axis
+      svg.append('g')
+        .call(d3.axisLeft(y))
+
+      // define the line
+      let line = d3.line()
+        .x(d => x(d[0]))
+        .y(d => y(d[1]))
+        .curve(d3.curveMonotoneX)
+        
+      // add the line
+      svg.append("g")
+        .attr('class', 'lineContainer')
+        .append('path')
+        .data([bufferline])
+        .attr('class', 'line')
+        .attr('d', line)
+        .attr('fill', 'none')
+        .attr('stroke', 'steelblue')
+        .attr('stroke-width', '1.5')
+        
+      // add the dots  
+      svg.selectAll('.lineContainer').selectAll('circle')
+        .data(bufferline).enter()
+        .append('circle')
+        .attr('class', 'dot')
+        .attr('cx', d => x(d[0]))
+        .attr('cy', d => y(d[1]))
+        .attr('r', '3')
+        .attr('fill', 'steelblue')  
+
+    } 
     
   }
 
